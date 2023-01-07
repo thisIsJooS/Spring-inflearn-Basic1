cf963fe727cacdd39cf704325f2296649562aefc
thisIsJooS <thisis.joos@gmail.com>
2023-01-07 11:46:58 +0900

    lombok

--- src/main/java/joos/core/member/MemberServiceImpl.java
+++ src/main/java/joos/core/member/MemberServiceImpl.java
@@ -6,13 +6,9 @@ import org.springframework.stereotype.Component;
 
 
 @Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
-
-    @Autowired
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     @Override
     public void join(Member member) {
--- src/main/java/joos/core/order/OrderServiceImpl.java
+++ src/main/java/joos/core/order/OrderServiceImpl.java
@@ -11,16 +11,11 @@ import org.springframework.stereotype.Component;
 
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
