d000746fa9b147a842bdeedef51c5553242554df
thisIsJooS <thisis.joos@gmail.com>
2023-01-13 00:56:49 +0900

    request scope 빈에서 proxyMode

--- src/main/java/joos/core/web/LogDemoController.java
+++ src/main/java/joos/core/web/LogDemoController.java
@@ -13,14 +13,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-
-    //ObjectProvider.getObject()를 호출하는 시점까지 request scope 빈의 생성을 지연할 수 있다.
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("/log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
